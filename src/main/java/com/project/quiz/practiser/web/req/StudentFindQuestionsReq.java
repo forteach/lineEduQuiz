14282539ea755aba5e58ae90ac7617bbcc9f8ff7
ang00 <zhang10092009@hotmail.com>
2019-09-11 19:34:34 +0800

    fix findQuestions

--- src/main/java/com/project/quiz/practiser/web/req/StudentFindQuestionsReq.java
+++ src/main/java/com/project/quiz/practiser/web/req/StudentFindQuestionsReq.java
@@ -1,6 +1,7 @@
 package com.project.quiz.practiser.web.req;
 
 import io.swagger.annotations.ApiModelProperty;
+import lombok.Builder;
 import lombok.Data;
 
 import java.io.Serializable;
@@ -17,25 +18,28 @@ import static com.project.quiz.practiser.constant.Dic.STUDENT_QUESTIONS;
 @Data
 public class StudentFindQuestionsReq implements Serializable {
 
-    @ApiModelProperty(name = "courseId", value = "课程id", dataType = "string", required = true)
-    private String courseId;
+//    @ApiModelProperty(name = "courseId", value = "课程id", dataType = "string", required = true)
+//    private String courseId;
 
     @ApiModelProperty(name = "chapterId", value = "章节id", dataType = "string", required = true)
     private String chapterId;
 
-    @ApiModelProperty(name = "chapterName", value = "章节名称", dataType = "string", required = true)
-    private String chapterName;
+//    @ApiModelProperty(name = "chapterName", value = "章节名称", dataType = "string", required = true)
+//    private String chapterName;
 
     @ApiModelProperty(name = "number", value = "随机题目数量", dataType = "int", required = true)
-    private Integer number;
+    private Integer number = 5;
 
-    @ApiModelProperty(name = "classId", value = "班级id", dataType = "string")
-    private String classId;
+//    @ApiModelProperty(name = "classId", value = "班级id", dataType = "string")
+//    private String classId;
 
     @ApiModelProperty(hidden = true)
     private String studentId;
 
     public String getKey(){
-        return STUDENT_QUESTIONS.concat(studentId).concat("#").concat(courseId).concat("#").concat(chapterId);
+//        return STUDENT_QUESTIONS.concat(studentId).concat("#").concat(courseId).concat("#").concat(chapterId);
+//        return STUDENT_QUESTIONS.concat(studentId).concat("#").concat(courseId).concat("#").concat(chapterId);
+//        return STUDENT_QUESTIONS.concat(studentId).concat("#").concat(courseId).concat("#").concat(chapterId);
+        return STUDENT_QUESTIONS.concat(studentId).concat("#").concat(chapterId);
     }
 }
